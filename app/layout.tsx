87f6a08fd51ac02c3dae8925e7b2ff94097252dd
catdevdog <catdevdog@gmail.com>
2024-06-12 11:31:41 +0900

    fix: Add Mobile Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,8 @@ export default function RootLayout({
     <html lang="ko">
       <Head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
       </Head>
