bd4de15b91d66ecf74646829815453a69255c5fe
catdevdog <catdevdog@gmail.com>
2024-06-12 11:21:52 +0900

    feat: Add Mobile favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import Head from 'next/head';
 import "./globals.css";
 
 declare global {
@@ -19,7 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+      </Head>
       <body className="text-black">{children}</body>
     </html>
   );
