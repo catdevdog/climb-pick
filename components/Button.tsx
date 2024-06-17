c08e1e27eb87dd41bd3ad03e98f0e1da80c78742
catdevdog <catdevdog@gmail.com>
2024-06-17 13:46:09 +0900

    feat: #19 Button 컴포넌트 아이콘 타입 추가

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,39 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 type ButtonProps = {
-    color: 'black' | 'white' | 'primary' | 'secondary' | 'danger' | 'warning' | 'info';
-    size: 'small' | 'medium' | 'large';
-    border?: boolean
-    rounded?: boolean
-    onClick: () => void;
-    children: React.ReactNode;
-    className?: string;
+  color:
+    | "black"
+    | "white"
+    | "primary"
+    | "secondary"
+    | "danger"
+    | "warning"
+    | "info";
+  size: "small" | "medium" | "large";
+  border?: boolean;
+  rounded?: boolean;
+  onClick: () => void;
+  children: React.ReactNode;
+  className?: string;
+  icon?: boolean;
 };
 
-export default function Button({ color, size, rounded = true, border = false, onClick, children, className }: ButtonProps) {
-    const _layout = `${rounded ? 'rounded-full' : ''} ${border ? 'border' : ''} ${className}`
+export default function Button({
+  color,
+  size,
+  rounded = true,
+  border = false,
+  icon = false,
+  onClick,
+  children,
+  className,
+}: ButtonProps) {
+  const _base = `${rounded ? "rounded-full" : ""} ${
+    border ? "border" : ""
+  } ${className}`;
 
-    const _size = {
-        small: 'text-xs py-2 px-3',
-        medium: 'text-md py-2 px-4',
-        large: 'text-lg py-2 px-4'
-    }[size];
+  const _size = {
+    small: `text-xs ${!icon ? "py-2 px-3" : "p-2"}`,
+    medium: `text-md ${!icon ? "py-2 px-4" : "p-2"}`,
+    large: `text-lg  ${!icon ? "py-2 px-4" : "p-2"}`,
+  }[size];
 
-    const _color = {
-        black: `bg-black text-white`,
-        white: `bg-white text-black border`,
-        primary: `bg-black text-white`,
-        secondary: `bg-white text-${color} border border-${color}`,
-        danger: `bg-red-500 text-white`,
-        warning: `bg-yellow-500 text-white`,
-        info: `bg-blue-500 text-white`,
-    }[color];
+  const _color = {
+    black: `bg-black text-white`,
+    white: `bg-white text-black border`,
+    primary: `bg-black text-white`,
+    secondary: `bg-white text-wtite border`,
+    danger: `bg-red-500 text-white`,
+    warning: `bg-yellow-500 text-white`,
+    info: `bg-blue-500 text-white`,
+  }[color];
 
-    const _class = `${_layout} ${_color} ${_size}`
+  const _icon = icon && "flex items-center justify-center";
 
-    return (
-        <button className={_class} onClick={onClick}>
-            {children}
-        </button>
-    );
-};
+  const _class = `${_base} ${_color} ${_size} ${_icon}`;
+
+  return (
+    <button className={_class} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
