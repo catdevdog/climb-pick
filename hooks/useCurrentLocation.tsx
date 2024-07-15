b07c584ffe85276f8e5416c516b213d4f6e90288
catdevdog <catdevdog@gmail.com>
2024-07-15 15:25:46 +0900

    fix: 모바일 현재 위치 함수 수정

--- hooks/useCurrentLocation.tsx
+++ hooks/useCurrentLocation.tsx
@@ -32,7 +32,7 @@ const useCurrentLocation = (): UseCurrentLocationResult => {
             (resolve, reject) => {
               navigator.geolocation.getCurrentPosition(resolve, reject, {
                 enableHighAccuracy: true,
-                timeout: 5000,
+                timeout: 10000,
                 maximumAge: 0,
               });
             }
